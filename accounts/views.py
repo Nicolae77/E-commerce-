17a7104a61a0867c74e40b518ed33f61410d8748
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2022-06-23 11:00:37 +0100

    Redirect User to next Page

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
+import requests
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -94,7 +96,15 @@ def login(request):
                 pass
             auth.login(request, user)
             messages.success(request, "You are now loged in.")
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, "Invalid login credentials.")
             return redirect('login')
