cca3f293b2557ee66e23ef1dce3bddd12b39d969
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2022-06-30 12:22:52 +0100

    Rating Average Calculation

--- store/views.py
+++ store/views.py
@@ -42,10 +42,10 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
 
-    try:
-        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
-    except OrderProduct.DoesNotExist:
-        orderproduct = None
+    # try:
+    #     orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+    # except OrderProduct.DoesNotExist:
+    #     orderproduct = None
 
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
@@ -53,7 +53,7 @@ def product_detail(request, category_slug, product_slug):
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
-        'orderproduct': orderproduct,
+        # 'orderproduct': orderproduct,
         'reviews': reviews,
     }
     return render(request, 'store/product_detail.html', context)
