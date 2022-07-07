508a96a6c51d46e41e5a72258ac53a08fb4c3849
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2022-07-07 11:27:18 +0100

    User Profile Modified

--- accounts/admin.py
+++ accounts/admin.py
@@ -13,4 +13,11 @@ class AccountAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
 
+# class UserProfileAdmin(admin.ModelAdmin):
+#     def thumbmail(self, object):
+#         return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
+#     thumbmail.short_description = 'Profile Picture'
+#     list_display = ('thumbmail', 'user', 'city', 'state', 'country')
+
 admin.site.register(Account, AccountAdmin)
+# admin.site.register(UserProfile, UserProfileAdmin)
